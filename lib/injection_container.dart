0fbae40240ce3fe8582b04a65b77e3f791502e2e
Gustavo G <soulandbone@gmail.com>
2025-06-03 14:23:20 -0400

    Eliminated Type Definition in Injection Container, except where it was absolutely needed

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -11,44 +11,12 @@ import 'package:get_it/get_it.dart';
 final getIt = GetIt.instance;
 
 void setup() {
-  getIt.registerLazySingleton<FirebaseFirestore>(
-    () => FirebaseFirestore.instance,
-  );
-  getIt.registerLazySingleton(
-    () => FirebaseQuizDataSource(getIt<FirebaseFirestore>()),
-  );
+  getIt.registerLazySingleton(() => FirebaseFirestore.instance);
+  getIt.registerLazySingleton(() => FirebaseQuizDataSource(getIt()));
   getIt.registerLazySingleton<QuizRepository>(
     () => QuizRepositoryImpl(getIt<FirebaseQuizDataSource>()),
   );
   getIt.registerLazySingleton(() => GetQuestions(repository: getIt()));
-  getIt.registerLazySingleton<CheckAnswer>(() => CheckAnswer());
-  getIt.registerLazySingleton<UpdateScore>(() => UpdateScore());
+  getIt.registerLazySingleton(() => CheckAnswer());
+  getIt.registerLazySingleton(() => UpdateScore());
 }
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:der_die_das/data/datasources/firebase_quiz_data_source.dart';
-// import 'package:der_die_das/data/repositories/quiz_repository_impl.dart';
-// import 'package:der_die_das/domain/repositories/quiz_repository.dart';
-
-// import 'package:der_die_das/domain/usecases/check_answer.dart';
-// import 'package:der_die_das/domain/usecases/get_questions.dart';
-// import 'package:der_die_das/domain/usecases/update_score.dart';
-// import 'package:get_it/get_it.dart';
-
-// final getIt = GetIt.instance;
-
-// void setup() {
-//   getIt.registerLazySingleton<FirebaseFirestore>(
-//     () => FirebaseFirestore.instance,
-//   );
-//   getIt.registerLazySingleton(
-//     () => FirebaseQuizDataSource(getIt<FirebaseFirestore>()),
-//   );
-//   getIt.registerLazySingleton<QuizRepository>(
-//     () => QuizRepositoryImpl(getIt<FirebaseQuizDataSource>()),
-//   );
-//   getIt.registerLazySingleton(() => GetQuestions(repository: getIt()));
-//   getIt.registerLazySingleton<CheckAnswer>(() => CheckAnswer());
-//   getIt.registerLazySingleton<UpdateScore>(() => UpdateScore());
-// }
